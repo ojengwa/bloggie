a6bb6dcefad65f9e5a1d1631a3bad8dd86ae3f9f
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-03-14 20:53:02 +0100

    base bootsrap

--- blogs/models.py
+++ blogs/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-  author_name = models.charField(max_length=50)
+  author_name = models.CharField(max_length=50)
   date_joined = models.DateTimeField('date joined')
   #"""docstring for Author"""
   # def __init__(self, arg):
   #   super(Author, self).__init__()
   #   self.arg = arg
-  #   
+  #
 
 class Post(models.Model):
   author = models.ForeignKey(Author)
-  text = models.charField(max_length=900)
+  text = models.CharField(max_length=100)
   likes = models.IntegerField(default=0)
   #"""docstring for Post"""
   # def __init__(self, arg):
@@ -21,11 +21,11 @@ class Post(models.Model):
 
 class Comment(models.Model):
   post = models.ForeignKey(Post)
-  comment = models.charField(max_length=900)
+  comment = models.CharField(max_length=900)
 
   # """docstring for Comment"""
   # def __init__(self, arg):
   #   super(Comment, self).__init__()
   #   self.arg = arg
-    
-  #   
+
+  #
